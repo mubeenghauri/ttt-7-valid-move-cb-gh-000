28e58269e0ad9151b1e2c3d75a8ea08309da3928
Mubeen Ghauri <ghauri.mubeen@gmail.com>
2019-01-19 12:25:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,9) and (position_taken(board, index) == false)
+  if index.between?(0,9) and (position_taken?(board, index) == false)
     return true
   else
     return false
